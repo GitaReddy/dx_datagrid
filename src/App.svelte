7b66e39a60ce2eff805fc5d82f52d35d4218b31c
geethasree.puli <geethasree.puli@gmail.com>
2023-06-08 21:18:42 +0530

    dxdatagrid_done

--- src/App.svelte
+++ src/App.svelte
@@ -1,55 +1,60 @@
+<script>
+	import { onMount } from "svelte";
+	// Sample data
+	let jsonData = [];
+	let data = [];
 
-    
+	onMount(async () => {
+		const response = await fetch(
+			"https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154"
+		);
+		const responseData = await response.json();
+		jsonData = responseData.data;
+		console.log(jsonData, "jsonsdsfs");
+		const gridData = jsonData.map(item => ({
+    //   id: item.id,
+	reference:item.reference,
+      name: item.fullName,
+	  email:item.email,
+	  phone:item.mobile,
 
-    
 
-    <script>
-		import { onMount } from 'svelte';
-        // Sample data
-		let jsonData = [];
+      // map other properties accordingly
+    }));
+console.log(gridData,"griddata");
+	// });
+
+	var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
+		dataSource: gridData,
+		columns: [
+        { dataField: 'reference', caption: 'ID' },
+        { dataField: 'name', caption: 'Name' },
+		{ dataField: 'email', caption: 'Email' },
+		{ dataField: 'phone', caption: 'Mobile' },
+        // Define other columns as needed
+      ],
+
 		
-		
-		onMount(async () => {
-    // try {
-      const response = await fetch("https://api.recruitly.io/api/candidate?apiKey=TEST9349C0221517DA4942E39B5DF18C68CDA154");
-      jsonData = await response.json();
-	  console.log(jsonData,"jsonsdsfs");
-    // } 
-	// catch (error) {
-    //   console.error('Error fetching data:', error);
-    // }
-  
-		
-        // Initialize the data grid
-        var dataGrid = new DevExpress.ui.dxDataGrid("#dataGrid", {
-            dataSource: jsonData,
-            columns: [
-                // { dataField: 'ID', caption: 'ID' },
-                { dataField: 'jsonData.firstName', caption: 'Name1' },
-                // { dataField: 'Age', caption: 'Age' }
-                // Add more columns as needed
-            ],
-            showBorders: true,
-            filterRow: {
-                visible: true
-            },
-            editing: {
-                allowUpdating: true,
-                allowDeleting: true,
-                allowAdding: true
-            },
-            paging: {
-                pageSize: 10
-            },
-            pager: {
-                showPageSizeSelector: true,
-                allowedPageSizes: [5, 10, 20],
-                showInfo: true
-            }
-        });
+		showBorders: true,
+		filterRow: {
+			visible: true,
+		},
+		editing: {
+			
+			allowUpdating: true,
+			allowDeleting: true,
+			allowAdding: true,
+		},
+		paging: {
+			pageSize: 10,
+		},
+		pager: {
+			showPageSizeSelector: true,
+			allowedPageSizes: [5, 10, 20],
+			showInfo: true,
+		},
 	});
-        // Render the data grid
-        // dataGrid.render();
-    </script>
-
-
+	});
+	// dataGrid.render();
+	// });
+</script>
